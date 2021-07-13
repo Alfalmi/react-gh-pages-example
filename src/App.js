99f5ab8fb8da9c41f771ca5e8f32c89750f49095
Alfalmi <pinooxd@gmail.com>
2021-07-13 16:35:05 -0700

    esp

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Aplicattion - This is my app</h1>
+        <h1>React Aplicattion - Esta es mi App</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
